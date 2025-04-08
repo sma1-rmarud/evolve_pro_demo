f63e7a28c088e0470c8f5c31eaec712c4c423121
abzb1 <cxv0519@gmail.com>
2025-04-08 08:32:25 +0000

    black & isort

--- biogui/app.py
+++ biogui/app.py
@@ -1,21 +1,24 @@
 import gradio as gr
 
-from biogui.utils.evolvepro_utils import predict_structure
+from biogui.utils.evolvepro_utils import predict_evolvepro
+
 
 def predict(f_type, structure_files):
     if not structure_files:
         return gr.Error("File upload failed.")
-    
+
     file_names = [f.name for f in structure_files]
     for file_name in file_names:
         if f_type == 'CIF' and not file_name.endswith(".cif"):
             gr.Error("The file you uploaded is not in CIF format 💥!")
         elif f_type == 'PDB' and not file_name.endswith(".pdb"):
             gr.Error("The file you uploaded is not in PDB format 💥!")
-        
+
+
 def upload_file(files):
     file_paths = [file.name for file in files]
     return file_paths
+
 
 with gr.Blocks() as demo:
     gr.Markdown(
@@ -24,57 +27,58 @@ with gr.Blocks() as demo:
         🧬ES activity prediction using Deep Learning
         </h1>
         """,
-        elem_id = "title"
+        elem_id="title",
     )
 
     with gr.Tab(label='EvolvePro'):
-        with gr.Row(variant='panel'):
-            with gr.Column(scale=4):
-                file_format_choice = gr.Radio(
-                            ["PDB", "CIF"], 
-                            label="File format", 
-                            info="Choose your structure file format"
-                        )
-            with gr.Column(scale=6):
-                file_output = gr.Files(file_types=[".cif", ".pdb"])
-                upload_button = gr.UploadButton("Click to Upload a File", file_types=[".cif", ".pdb"], file_count="multiple")
-                upload_button.upload(upload_file, upload_button, file_output)
-        
+        with gr.Column(scale=4):
+            file_format_choice = gr.Radio(
+                ["PDB", "CIF"],
+                label="File format",
+                info="Choose your structure file format",
+            )
+        with gr.Column(scale=6):
+            file_output = gr.Files(file_types=[".cif", ".pdb"])
+            upload_button = gr.UploadButton(
+                "Click to Upload a File",
+                file_types=[".cif", ".pdb"],
+                file_count="multiple",
+            )
+            upload_button.upload(upload_file, upload_button, file_output)
+
         output_text = gr.Textbox(label="Predicted activity label")
         predict_button = gr.Button("predict")
-        
+
         predict_button.click(
-            fn=predict,
-            inputs=[
-                file_format_choice,
-                file_output
-            ],
-            outputs=[output_text]
+            fn=predict_evolvepro,
+            inputs=[file_format_choice, file_output],
+            outputs=[output_text],
         )
 
     with gr.Tab(label='Ours'):
         with gr.Row(variant='panel'):
             with gr.Column(scale=4):
                 file_format_choice = gr.Radio(
-                            ["PDB", "CIF"], 
-                            label="File format", 
-                            info="Choose your structure file format"
-                        )
+                    ["PDB", "CIF"],
+                    label="File format",
+                    info="Choose your structure file format",
+                )
             with gr.Column(scale=6):
                 file_output = gr.Files(file_types=[".cif", ".pdb"])
-                upload_button = gr.UploadButton("Click to Upload a File", file_types=[".cif", ".pdb"], file_count="multiple")
+                upload_button = gr.UploadButton(
+                    "Click to Upload a File",
+                    file_types=[".cif", ".pdb"],
+                    file_count="multiple",
+                )
                 upload_button.upload(upload_file, upload_button, file_output)
-        
+
         output_text = gr.Textbox(label="Predicted activity label")
         predict_button = gr.Button("predict")
-        
+
         predict_button.click(
             fn=predict,
-            inputs=[
-                file_format_choice,
-                file_output
-            ],
-            outputs=[output_text]
+            inputs=[file_format_choice, file_output],
+            outputs=[output_text],
         )
 
 demo.launch()
--- biogui/utils/evolvepro_utils.py
+++ biogui/utils/evolvepro_utils.py
@@ -1,10 +1,16 @@
-import sys
 import os
+import sys
+
+from evolvepro.src.process import generate_single_aa_mutants, generate_wt
+
 # sys.path.append(os.path.join(os.path.dirname(__file__), "../external/evolvepro"))
 
-from evolvepro.src.process import generate_wt, generate_single_aa_mutants
 
 def predict_structure():
     pass
 
-generate_wt('ZKNVPQEPNZPCVZKNDINQORMVJNYUBNBIMZLZCXMVLQVPZXAUCYUBRMTVNZKDOKECOKPKQRNVV', output_file='kelsic_WT.fasta')
+
+generate_wt(
+    'ZKNVPQEPNZPCVZKNDINQORMVJNYUBNBIMZLZCXMVLQVPZXAUCYUBRMTVNZKDOKECOKPKQRNVV',
+    output_file='kelsic_WT.fasta',
+)
