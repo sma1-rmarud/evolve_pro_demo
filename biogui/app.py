ae6e3762cc7ac3fa3d7f16d84fcd51f4dc41be87
sma1-rmarud <rmarud202@uos.ac.kr>
2025-04-09 23:49:12 +0000

    Update GUI and submodule launcher

--- biogui/app.py
+++ biogui/app.py
@@ -1,8 +1,15 @@
 import gradio as gr
+import tempfile
+import os
 
 from biogui.utils.evolvepro_utils import predict_evolvepro
 from biogui.utils.gnn_utils import predict_gnn
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+custom_tmp = os.path.join(BASE_DIR, "tmp_gradio")
+os.makedirs(custom_tmp, exist_ok=True)
+tempfile.tempdir = custom_tmp
+os.environ["GRADIO_TEMP_DIR"] = custom_tmp
 
 def upload_file(files):
     file_paths = [file.name for file in files]
@@ -76,30 +83,30 @@ with gr.Blocks() as demo:
             outputs=[output_img],
         )
 
-    with gr.Tab(label='Ours'):
-        with gr.Row(variant='panel'):
-            with gr.Column(scale=4):
-                file_format_choice = gr.Radio(
-                    ["PDB", "CIF"],
-                    label="File format",
-                    info="Choose your structure file format",
-                )
-            with gr.Column(scale=6):
-                file_output = gr.Files(file_types=[".cif", ".pdb"])
-                upload_button = gr.UploadButton(
-                    "Click to Upload a File",
-                    file_types=[".cif", ".pdb"],
-                    file_count="multiple",
-                )
-                upload_button.upload(upload_file, upload_button, file_output)
+    # with gr.Tab(label='Ours'):
+    #     with gr.Row(variant='panel'):
+    #         with gr.Column(scale=4):
+    #             file_format_choice = gr.Radio(
+    #                 ["PDB", "CIF"],
+    #                 label="File format",
+    #                 info="Choose your structure file format",
+    #             )
+    #         with gr.Column(scale=6):
+    #             file_output = gr.Files(file_types=[".cif", ".pdb"])
+    #             upload_button = gr.UploadButton(
+    #                 "Click to Upload a File",
+    #                 file_types=[".cif", ".pdb"],
+    #                 file_count="multiple",
+    #             )
+    #             upload_button.upload(upload_file, upload_button, file_output)
 
-        output_text = gr.Textbox(label="Predicted activity label")
-        predict_button = gr.Button("predict")
+    #     output_text = gr.Textbox(label="Predicted activity label")
+    #     predict_button = gr.Button("predict")
 
-        predict_button.click(
-            fn=predict_gnn,
-            inputs=[file_format_choice, file_output],
-            outputs=[output_text],
-        )
+    #     predict_button.click(
+    #         fn=predict_gnn,
+    #         inputs=[file_format_choice, file_output],
+    #         outputs=[output_text],
+    #     )
 
-demo.launch()
+demo.launch(debug=True)
