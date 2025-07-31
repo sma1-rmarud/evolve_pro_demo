5263c9f124d7685f082be758367692a2c047cb56
sma1-rmarud <a01085012013@gmail.com>
2025-07-31 04:44:11 +0000

    n-mutant combination generation

--- biogui/utils/evolvepro_utils.py
+++ biogui/utils/evolvepro_utils.py
@@ -112,88 +112,107 @@ def predict_evolvepro(
 
     
 def predict_n_mutants(
-    wt_seq,
-    mutant_files,
+    protein_name,
+    input_sequence,
+    s_mutant_file,
+    m_mutant_file,
     n_mutant,
     threshold,
     embedding_model,
     toks_per_batch,
     number_of_variants,
 ):
+    round_files = sorted([Path(file) for file in s_mutant_file])
+    if not m_mutant_file:
+        m_round_files = []
+    else:
+        m_round_files = sorted([Path(file) for file in m_mutant_file])
 
     with tempfile.TemporaryDirectory() as temp_dir:
-        wt_fasta = os.path.join(temp_dir, "WT.fasta")
-        generate_wt(wt_seq, wt_fasta)
-        
+        logger.info(f"Creating Wildtype sequence file for {protein_name}")
+        wt_file = os.path.join(temp_dir, f"{protein_name}_WT.fasta")
+        generate_wt(input_sequence, wt_file)
+
+        logger.info(f"Creating single AA mutants for {protein_name}")
+        fasta_file = os.path.join(temp_dir, f"{protein_name}.fasta")
+        generate_single_aa_mutants(wt_file, fasta_file)
+
         round_data_dir = os.path.join(temp_dir, "rounds_data")
         os.makedirs(round_data_dir, exist_ok=True)
         
         embedding_dir = os.path.join(temp_dir, "embeddings")
         os.makedirs(embedding_dir, exist_ok=True)
         
-        processed_filenames = []
-        for f in mutant_files:
-            df = pd.read_excel(f.name)
-            if "Activity" in df.columns and "activity" not in df.columns:
-                df = df.rename(columns={"Activity": "activity"})
-            df = df[["Variant", "activity"]]
-            file_basename = Path(f.name).name
-            output_path = os.path.join(round_data_dir, file_basename)
-            df.to_excel(output_path, index=False)
-            processed_filenames.append(file_basename)
-
-        single_mutant_fasta = os.path.join(temp_dir, "single_mutants.fasta")
-        generate_single_aa_mutants(wt_fasta, single_mutant_fasta)
+        merged_df_list = []
+        merged_output_path = os.path.join(round_data_dir, "merged_mutants.xlsx")
         
+        for f in round_files:
+            df = pd.read_excel(f)
+            df = df[["Variant", "activity"]]
+            
+            merged_df_list.append(df)
+
+        if merged_df_list:
+            merged_df = pd.concat(merged_df_list, ignore_index=True)
+            merged_df.to_excel(merged_output_path, index=False)
+            
+        concatenate_dir = os.path.join(temp_dir, "output")
         args_single = Namespace(
-            model_location=embedding_model, fasta_file=single_mutant_fasta,
+            model_location=embedding_model,
+            fasta_file=fasta_file,
             output_dir=Path(os.path.join(embedding_dir, "single_mutants_emb")),
-            toks_per_batch=int(toks_per_batch), repr_layers=[-1], include=["mean"],
-            truncation_seq_length=1022, nogpu=False,
-            concatenate_dir=embedding_dir,
+            toks_per_batch=toks_per_batch,
+            repr_layers=[-1],
+            include=["mean"],
+            truncation_seq_length=1022,
+            nogpu=False,
+            concatenate_dir=concatenate_dir,
         )
         extract_embeddings(args_single)
         
-        single_fasta_name = Path(single_mutant_fasta).stem
+        single_fasta_name = f"single_{protein_name}_fasta"
         single_embedding_csv_name = f"{single_fasta_name}_{embedding_model}.csv"
         concatenate_files(args_single.output_dir, os.path.join(embedding_dir, single_embedding_csv_name))
         
-        latest_filepath = os.path.join(round_data_dir, sorted(processed_filenames)[-1])
         n_mutant_fasta = os.path.join(temp_dir, f"dataset_{n_mutant}th.fasta")
         generate_n_mutant_combinations(
-            wt_fasta, latest_filepath, n=n_mutant,
+            wt_file, merged_output_path, n=n_mutant,
             output_file=n_mutant_fasta, threshold=threshold,
         )
-
+        
         args_n_mutant = Namespace(
-            model_location=embedding_model, fasta_file=n_mutant_fasta,
+            model_location=embedding_model,
+            fasta_file=n_mutant_fasta,
             output_dir=Path(os.path.join(embedding_dir, "n_mutants_emb")),
-            toks_per_batch=int(toks_per_batch), repr_layers=[-1], include=["mean"],
-            truncation_seq_length=1022, nogpu=False,
-            concatenate_dir=embedding_dir,
+            toks_per_batch=toks_per_batch,
+            repr_layers=[-1],
+            include=["mean"],
+            truncation_seq_length=1022,
+            nogpu=False,
+            concatenate_dir=concatenate_dir,
         )
         extract_embeddings(args_n_mutant)
-        
-        n_mutant_fasta_name = Path(n_mutant_fasta).stem
+
+        n_mutant_fasta_name = f"n_mutant_{protein_name}_fasta"
         n_mutant_embedding_csv_name = f"{n_mutant_fasta_name}_{embedding_model}.csv"
         concatenate_files(args_n_mutant.output_dir, os.path.join(embedding_dir, n_mutant_embedding_csv_name))
 
         this_round_variants, df_test, df_sorted_all = evolve_experimental_multi(
-            protein_name="N_Mutant",
+            protein_name=protein_name,
             round_name=f"Round_n{n_mutant}",
             embeddings_base_path=embedding_dir,
             embeddings_file_names=[single_embedding_csv_name, n_mutant_embedding_csv_name],
             round_base_path=round_data_dir,
-            round_file_names_single=processed_filenames,
-            round_file_names_multi=[],
-            wt_fasta_path=wt_fasta,
+            round_file_names_single=round_files,
+            round_file_names_multi=m_round_files,
+            wt_fasta_path=wt_file,
             rename_WT=False,
             number_of_variants=int(number_of_variants),
             output_dir=embedding_dir,
         )
         img_output_dir = os.path.join(temp_dir, "output", "images")
-        img_output_file = f"N_Mutant_{embedding_model}.png"
-        df = read_exp_data(round_data_dir, processed_filenames, wt_fasta)
+        img_output_file = f"N_Mutant_{protein_name}_{embedding_model}.png"
+        df = read_exp_data(round_data_dir, round_files, wt_file)
         plot_variants_by_iteration(
             df,
             activity_column='activity',
