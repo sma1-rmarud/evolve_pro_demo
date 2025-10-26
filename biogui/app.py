bddaf747189a81a03d1fb6fb532c7968f691d310
sma1-rmarud <a01085012013@gmail.com>
2025-10-26 10:38:55 +0000

    modify app.py

--- biogui/app.py
+++ biogui/app.py
@@ -6,7 +6,6 @@ import pandas as pd
 import gradio as gr
 
 from biogui.utils.evolvepro_utils_v2 import predict_evolvepro, predict_n_mutants
-from biogui.utils.esm3_utils import run_ours
 
 with tempfile.TemporaryDirectory() as gradio_tmp:
     with gr.Blocks() as demo:
@@ -165,36 +164,6 @@ with tempfile.TemporaryDirectory() as gradio_tmp:
                 ],
                 outputs=[
                     output_img, output_table_variants, output_table_test, output_table_sorted
-                ]
-            )
-
-
-        with gr.Tab(label='Run_ours_esm3_finetuned'):
-            gr.Markdown("## 🔬 Predict activity score with our model")
-            protein_name = gr.Textbox(
-                label="protein name", placeholder="Enter protein name"
-            )
-            input_sequence_wt = gr.Textbox(label="Wildtype Sequence", placeholder="MMA...")
-            ligand_name = gr.Textbox(
-                label="ligand name", placeholder="Enter ligand name", value=None
-            )
-            ligand_smiles = gr.Textbox(label="Smiles code of ligand", placeholder="CC1([C@...", value=None)
-            
-            csv_file = gr.File(
-                file_types=[".csv"],
-                label="files to predict"
-            )
-            
-            output_csv = gr.File(label="Download predictions CSV")
-            
-            predict_button = gr.Button("generate and run")
-            predict_button.click(
-                fn=run_ours,
-                inputs=[
-                    protein_name, input_sequence_wt, ligand_name, ligand_smiles, csv_file
-                ],
-                outputs=[
-                    output_csv
                 ]
             )
 
